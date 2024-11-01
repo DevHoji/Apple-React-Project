97fae9a04b58a80ca524f08ff83508ec88a7c075
yabselam <yabsirahoji@gmail.com>
2024-11-01 08:45:44 -0700

    apple-react project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import FifthSection from './components/FifthSection'
+import FirstSection from './components/FirstSection'
+import Footer from './components/footer'
+import FourthSection from './components/FourthSection'
+import Header from './components/Header'
+import SecondSection from './components/SecondSection'
+import SixthSection from './components/SixthSection'
+import ThirdSection from './components/ThirdSection'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <FirstSection/>
+    <SecondSection/>
+    <ThirdSection/>
+    <FourthSection/>
+    <FifthSection/>
+    <SixthSection/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
